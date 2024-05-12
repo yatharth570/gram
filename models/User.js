3a2f642000dc7fc774a4a4159f5d4a7b94697614
yatharth <yatharth570@gmail.com>
2024-05-12 12:25:32 +0530

    changed user defination function defination and return

--- models/User.js
+++ models/User.js
@@ -1,40 +1,35 @@
-const { Sequelize } = require("sequelize");
-const sequelize = require('../db')
-module.exports=(sequelize,DataTypes)=>{
-    const User = sequelize.define(
-        'user',
-        {
-          // Model attributes are defined here
-          firstName: {
-            type: Sequelize.STRING,
-            allowNull: false,
-          },
-          lastName: {
-            type: Sequelize.STRING,
-            allowNull: false
-          },
-          username:{
-            type: Sequelize.STRING,
-            allowNull: false,
-            unique: true
-          },
-          email:{
-            type:Sequelize.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true,
-              },
-          },
-          password_hash:{
-            type: Sequelize.STRING,
-            allowNull: false
-          },
-          create_timestamp:{
-            type: Sequelize.DATE, 
-            defaultValue: DataTypes.NOW 
-          }
-        }
-      );
-      return User;
-}
+const  {sequelize}  = require("../db");
+const { Sequelize } = require('sequelize')
+const User = sequelize.define(
+  'user',
+  {
+    // Model attributes are defined here
+    firstName: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    username:{
+      type: Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      primaryKey:true,
+    },
+    email:{
+      type:Sequelize.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+          isEmail: true,
+        },
+    },
+    password_hash:{
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+  }
+);
+module.exports = User;
